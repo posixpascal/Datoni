4d81cc0f59c2a96078e2381c4c30d75d9b16a42e
Pascal Raszyk <pr@mediatrooper.de>
2016-10-17 20:12:19 +0200

    added new route

--- controllers/contact.js
+++ controllers/contact.js
@@ -9,35 +9,11 @@ module.exports = function(server) {
 
     });
 
-    server.post('/kontakt/send', function(req, res) {
-        var name = req.body.name;
-        var email = req.body.email;
-        var message = req.body.message;
-        var _post = true;
-        
-        // post validation
-        if (name === '' || typeof name === 'undefined' || name.length < 2) {
-            _post = false;
-            req.flash('error', 'Bitte gebe einen Namen an. (Mindestens 2 Buchstaben)');
-        }
-
-        if (email === '' || typeof email === 'undefined' || email.length < 4 || !/(.*)@(.+)\.(.*)/ig.test(email)) {
-            _post = false;
-            req.flash('error', 'Bitte gebe eine E-Mail Adresse an, damit wir dich kontaktieren können');
-        }
-
-        if (message === '' || typeof message === 'undefined' || message.length < 2){
-            _post = false;
-            req.flash('error', 'Bitte gebe eine Nachricht an, welche an uns geschickt werden soll');
-        }
-
-        if (_post){ 
-            // send mail
+    server.get('/kontakt/send', function(req, res) {
+       
             req.flash('success', 'Deine Nachricht wurde erfolgreich verschickt!');
             res.redirect('/');
-        } else {
-            res.redirect('/kontakt');
-        }
+     
 
        
 
